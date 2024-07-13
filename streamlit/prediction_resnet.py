3ed57bc648f33b368f01f3ed7de6e0a9e9f18a0f
waheed <142170637+wfaiza@users.noreply.github.com>
2024-07-14 01:15:10 +0200

    Update prediction_resnet.py

--- streamlit/prediction_resnet.py
+++ streamlit/prediction_resnet.py
@@ -235,7 +235,7 @@ def process_image(num, test_img):
                 min_contour_area = total_area * 0.00027
                 max_contour_area = total_area * 0.0018 #0.0007
             elif 400000 < total_area <= 800000:
-                min_contour_area = total_area * 0.00045  # was 0.0005
+                min_contour_area = total_area * 0.0004  # was 0.0005
                 max_contour_area = total_area * 0.0022
             elif 200000 < total_area <= 400000:
                 min_contour_area = total_area * 0.0006
